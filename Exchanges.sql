c251d057af435068109afb62ab68f885667be0e5
CRYPTOX CEX <globalexcripto@gmail.com>
2025-08-03 15:52:31 +0100

    Create Exchanges.sql

---
+++ Exchanges.sql
@@ -0,0 +1,11 @@
+#**add this to your database using  sql to get ok, binance us ,kraken exchanges**
+
+
+INSERT INTO exchange (id, name, title, status, username, licenseStatus, version, productId, type) VALUES
+
+(4, 'binanceus', 'Binance US', '0', NULL, '0', '1.0.0', '2816DB47', 'spot'),
+
+(5, 'okx', 'OKX', '0', NULL, '0', '1.0.0', '34BDAB64', 'spot'),
+
+(6, 'kraken', 'Kraken', '0', NULL, '0', '1.0.0', 'AB56F8DE', 'spot');
+
