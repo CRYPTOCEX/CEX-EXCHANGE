31fa5f6757f048572a8bfdf6722c079adc57860c
CRYPTOX CEX <globalexcripto@gmail.com>
2026-09-20 17:06:13 +0100

    Delete backend directory

--- backend/dist/src/api/(ext)/admin/ai/market-maker/utils/engine/external/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExternalPriceSync = void 0;
-var ExternalPriceSync_1 = require("./ExternalPriceSync");
-Object.defineProperty(exports, "ExternalPriceSync", { enumerable: true, get: function () { return ExternalPriceSync_1.ExternalPriceSync; } });
--- backend/dist/src/api/exchange/binary/order/index.ws.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.metadata = void 0;
-exports.metadata = { requiresAuth: true };
-exports.default = async (data) => { };
